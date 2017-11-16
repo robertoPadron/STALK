cf0b9e1fb46439f66cc349e3adb1484d62197e5c
Unknown <guillerjack@hotmail.com>
2017-11-16 18:41:00 +0100

    Menu Principal
    Workin Progress

---
+++ STALK/Assets/SCRIPTS/CambiarMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CambiarMenu : MonoBehaviour {
+
+    public MenuMovil NumeroViejo;
+    public int NuevoNumero;
+    void OnMouseDown() {
+        NumeroViejo.NumeroPosicion = NuevoNumero;
+    }	
+}
---
+++ STALK/Assets/SCRIPTS/MENU/CambiarMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public MenuMovil NumeroViejo;
+    public int NuevoNumero;
+    void OnMouseDown() {
+
+        NumeroViejo.NumeroPosicion = NuevoNumero;
+    }
+	
+}
