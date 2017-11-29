7247cf9794fa96f9713f6b59345bb9fadb17d860
Unknown <guillerjack@hotmail.com>
2017-11-29 12:37:01 +0100

    Menu mejorado e inicio de nivel 2

---
+++ STALK/Assets/SCRIPTS/CambiarEscena.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CambiarEscena : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Character")
+        {
+            SceneManager.LoadScene("LEVEL_2");
+        }
+    }
+}
--- STALK/Assets/SCRIPTS/MENU/MenuMovil.cs
+++ STALK/Assets/SCRIPTS/MENU/MenuMovil.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuMovil : MonoBehaviour
 {
