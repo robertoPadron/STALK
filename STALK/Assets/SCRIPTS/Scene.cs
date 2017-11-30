52b93b386c2851ea4a9e1ec255e3f8b149253b62
Unknown <guillerjack@hotmail.com>
2017-11-30 14:27:27 +0100

    Progreso level1

---
+++ STALK/Assets/SCRIPTS/Scene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Scene : MonoBehaviour {
+
+    public void LoadScene(int level)
+    {
+        Application.LoadLevel(level);
+    }
+}
